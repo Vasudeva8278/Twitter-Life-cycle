4e256e2cde709b5dda427df5996a1bcb8a69069b
vasudev8278 <vasudevsanchapu@gmail.com>
2024-09-01 15:55:05 +0530

    commit

--- twitter_cycle/social/urls.py
+++ twitter_cycle/social/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import UserprofileDetailview,UserProfileListview,LikeDetailView,LikeListView,CommentDetailView,CommentListView,ShareDetailView,ShareListView,PostDetailView,PostListView,RegisterView,home_page_view,CustomerLogoutView
+from .views import UserprofileDetailview,UserProfileListview,LikeDetailView,LikeListView,CommentDetailView,CommentListView,ShareDetailView,ShareListView,PostDetailView,PostListView,RegisterView,home_page_view,CustomerLogoutView,CustomLoginView
 urlpatterns = [
     path('userlist/', UserProfileListview.as_view(),name='userlist'),
     path('reg/',RegisterView.as_view(),name='register'),
-    path('home/', home_page_view, name='home_page'),
+    path('home/', home_page_view, name='home'),  # Named URL pattern for home
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomerLogoutView.as_view(), name='logout'),
     path('profiledetails/<str:user__username>/',UserprofileDetailview.as_view(),name='profile_details'),
     path('Posts/<str:user__username>',PostListView.as_view(),name="posts"),
